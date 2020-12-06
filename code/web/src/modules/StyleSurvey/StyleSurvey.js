0f7b39d8102a2c9cd5c957f88317e00a65c0ae64
Orlando Murcio <ato.murcio@gmail.com>
2020-12-06 13:16:53 -0700

    Add thoughts on where to place the file

--- code/web/src/modules/StyleSurvey/StyleSurvey.js
+++ code/web/src/modules/StyleSurvey/StyleSurvey.js
@@ -1,4 +1,9 @@
+
+//This file could potentially bebetter pace inside the user folder 
+// and let the user/api/action and user/api/state handle the functionality 
+//of the survey insetad of creating a new entre api folder to handle the logic
 //import React from 'react'
+//import Hooks if we decide to use them
 
 //create a state sotre property for 
 //isSurveyFineshed = false
@@ -6,8 +11,14 @@
 //the sotre should be able to provide the app's state for
 //error, isLoading and isSurveyFinshed
 //declare the state of the form 
+
+//
 /*
 this.state = {
 
 }
+*/
+
+/*
+
 */
