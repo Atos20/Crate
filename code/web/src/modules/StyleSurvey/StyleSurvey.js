99331e6a045d72466ecec4edf2464e3a706f5ed2
Orlando Murcio <ato.murcio@gmail.com>
2020-12-06 13:10:09 -0700

    Create StyleSurvey file

---
+++ code/web/src/modules/StyleSurvey/StyleSurvey.js
@@ -0,0 +1,13 @@
+//import React from 'react'
+
+//create a state sotre property for 
+//isSurveyFineshed = false
+//create a ract functionalcompoentn called StyleCurvey => <StyledSurvey>
+//the sotre should be able to provide the app's state for
+//error, isLoading and isSurveyFinshed
+//declare the state of the form 
+/*
+this.state = {
+
+}
+*/
