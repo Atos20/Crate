d96cd607bb221ce4a28ac2f246d32eee047bc556
Orlando Murcio <ato.murcio@gmail.com>
2020-12-06 16:21:15 -0700

    Add list of potential components to use

--- code/web/src/modules/StyleSurvey/StyleSurvey.js
+++ code/web/src/modules/StyleSurvey/StyleSurvey.js
@@ -1,11 +1,15 @@
 
-//This file could potentially bebetter pace inside the user folder 
+//This file could potentially be  better placed inside the user folder 
 // and let the user/api/action and user/api/state handle the functionality 
-//of the survey insetad of creating a new entre api folder to handle the logic
-//import React from 'react'
-//import Hooks if we decide to use them
 
-//create a state sotre property for 
+
+// Double check with TextDecoderStream, because  this couls also be a different page???
+
+
+//need to import React from 'react'
+//need to import Hooks if we decide to use them
+
+//create a state tore property for 
 //isSurveyFineshed = false
 //create a ract functionalcompoentn called StyleCurvey => <StyledSurvey>
 //the sotre should be able to provide the app's state for
@@ -20,5 +24,15 @@ this.state = {
 */
 
 /*
-
+Reuse compoenent such as 
+<Grid> for container
+<GridCell> for cells
+<Item> for elements within the GridCell
+<Card> 
+<H1> Header
+<H3> Header
+<H4> Header
+<Button> well, fto click on something
+<Tile> for images
+<Modal> to render more information for the user if necessary
 */
