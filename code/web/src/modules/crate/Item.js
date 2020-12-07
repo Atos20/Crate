483094ee035be481bea05940cf931679cb52b722
Orlando Murcio <ato.murcio@gmail.com>
2020-12-06 19:16:23 -0700

    Add initial annotation about the items

--- code/web/src/modules/crate/Item.js
+++ code/web/src/modules/crate/Item.js
@@ -28,21 +28,22 @@ class Item extends PureComponent {
     }
   }
 
-  onClickSubscribe = (crateId) => {
+  onClickSubscribe = (crateId) => {//the user suscribes to a crate and changes the state.isLoading to true
     this.setState({
       isLoading: true
     })
 
     this.props.messageShow('Subscribing, please wait...')
+    //the state message gets updated
 
-    this.props.create({ crateId })
-      .then(response => {
-        if (response.data.errors && response.data.errors.length > 0) {
-          this.props.messageShow(response.data.errors[0].message)
+    this.props.create({ crateId })//activates a mutation to add a crate to the users subscription's list which is a []
+      .then(response => {//then returns the user's crates
+        if (response.data.errors && response.data.errors.length > 0) {//checks for erros 
+          this.props.messageShow(response.data.errors[0].message)// if they are any errrors then display then 
         } else {
-          this.props.messageShow('Subscribed successfully.')
+          this.props.messageShow('Subscribed successfully.')//if there are no errros the it was a suscessful subscription
 
-          this.props.history.push(userRoutes.subscriptions.path)
+          this.props.history.push(userRoutes.subscriptions.path)//a
         }
       })
       .catch(error => {
@@ -51,19 +52,20 @@ class Item extends PureComponent {
       .then(() => {
         this.setState({
           isLoading: false
-        })
+        })//after getting the crates then the app is no longer loadings
 
-        window.setTimeout(() => {
+        window.setTimeout(() => {//clears out the message in common/message.text
           this.props.messageHide()
         }, 5000)
       })
   }
 
   render() {
-    const { id, name, description } = this.props.crate
-    const { isLoading } = this.state
+    const { id, name, description } = this.props.crate//destructuring store  props
+    const { isLoading } = this.state // desctructuring component props
 
     return (
+      // <Card is a compoenent that renders a subscription>
       <Card style={{ width: '18em', backgroundColor: white }}>
         <p style={{ padding: '2em 3em 0 3em' }}>
           <img src={`${ APP_URL }/images/crate.png`} alt={name} style={{ width: '100%' }}/>
@@ -73,10 +75,14 @@ class Item extends PureComponent {
           <H4 font="secondary" style={{ color: black }}>{name}</H4>
 
           <p style={{ color: grey2, marginTop: '1em' }}>{description}</p>
+          {/*⬆️ rate descrioption*/}
 
           <p style={{ textAlign: 'center', marginTop: '1.5em', marginBottom: '1em' }}>
             <Button
               theme="primary"
+              // add a conditional when, if the user has not fillout the survey it will
+              //redirect to the Style surve other wise it will just add the crate to the 
+              //user's profile
               onClick={this.onClickSubscribe.bind(this, id)}
               type="button"
               disabled={ isLoading }
